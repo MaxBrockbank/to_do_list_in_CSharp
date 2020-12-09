0e22e83e36daccc8d793137ef9369c4d2873f59a
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-09 10:13:05 -0800

    finish unit testing
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,10 +5,24 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description {get;}
+
+    private static List<Item> _instances = new List<Item>{};
+    public static List<Item> GetAll(){
+      return _instances;
+    }
+
+    public string Description {get; set;}
     public Item (string description)
     {
       Description = description;
+      _instances.Add(this);
+      
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
+  
 }
